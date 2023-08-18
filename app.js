e518d4e10bbc7eb1b75e51ab3a4b6110c3da394f
Love Mittal <78198296+lovemittal01@users.noreply.github.com>
2023-08-18 20:46:25 +0530

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const db = require("./db");
 const crawler = require("./crawler.js");
 
 const app = express();
 app.use(express.json());
+app.use(cors({}));
 app.use(
   express.urlencoded({
     extended: true,
